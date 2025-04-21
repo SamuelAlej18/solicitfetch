ff282b1c42c09432b8ccfb878895daf6bc205c5b
SamuelAlej18 <mail.samuel.alejandro@gmail.com>
2025-04-21 09:18:19 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelector('button').addEventListener('click', () => {
     fetch('https://nodeyexpress.onrender.com/')
-    .then(response => response.json())
+    .then(response => response.text())
     .then (data => document.querySelector('h1').innerText=data)
     .catch(err => console.log(err))
 })
