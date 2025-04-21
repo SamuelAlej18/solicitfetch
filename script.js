60958840abb700582f49438d95f8be363a66c572
SamuelAlej18 <mail.samuel.alejandro@gmail.com>
2025-04-21 09:37:18 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('button').addEventListener('click', () => {
-    fetch('https://loginregister-5i1a.onrender.com/)
+    fetch('https://loginregister-5i1a.onrender.com/')
     .then(response => response.text())
     .then (data => document.querySelector('h1').innerText=data)
     .catch(err => console.log(err))
